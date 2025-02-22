e46b7074d0fe38f66dbf67822a6fd6891ef50681
Nathan Ma <nathanycma@gmail.com>
2025-02-22 15:38:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,7 @@ import pandas as pd
 st.title('🩺 Lung Sound Classification App')
 
 # Display a banner image comparing Spectrogram vs. PCEN
-st.image("https://raw.githubusercontent.com/IKnowUCantPvp/Lung-Class-Website/master/images/Spec_PCEN_Comparison.png", caption="Comparison of Spectrogram and PCEN")
-caption="Comparison of Spectrogram and PCEN", use_container_width=True)
+st.image("https://raw.githubusercontent.com/IKnowUCantPvp/Lung-Class-Website/master/images/Spec_PCEN_Comparison.png", caption="Comparison of Spectrogram and PCEN", use_container_width=True)
 
 st.markdown("""
 **About This Project**
