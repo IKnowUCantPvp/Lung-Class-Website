461797c977ced6fc8b2ca0df2bc9f09133c62153
Nathan Ma <nathanycma@gmail.com>
2025-02-22 12:03:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('🩺Lung Sound Classificaiton App')
 
-st.write('This app is built off of a research project researching the uasge of utilizing Per Channel Energy Normalization to classify lung sounds.!')
+st.write('This app is built off of a research project researching the uasge of utilizing Per Channel Energy Normalization to classify lung sounds!')
