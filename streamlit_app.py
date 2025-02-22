a316abf401607c4f2f368fd91dea1e8dbd31ea2e
IKnowUCantPvp <nathanycma@gmail.com>
2025-02-22 14:24:44 -0500

    added audio files

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.write(
 
 # Load your pre-trained model from the repository.
 # The model directory should contain the saved_model.pb file and related assets.
-MODEL_DIR = r"C:\Users\natha\OneDrive\Documents\GitHub\Lung-Class-Website\models\FinalModels\clean dataset\Conv2DOldPCEN"
+MODEL_DIR = "Conv2DOldPCEN"
 model = tf.saved_model.load(MODEL_DIR)
 # Get the default serving function from the model.
 inference_func = model.signatures["serving_default"]
